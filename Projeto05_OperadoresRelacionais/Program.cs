7b58774780d41919bda521a6d1b1500bbdcaa191
cfsilva28 <ceferreira@gmail.com>
2023-01-10 21:52:46 -0300

    Operadores Relacionais

---
+++ Projeto05_OperadoresRelacionais/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto05_OperadoresRelacionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Operadores Relacionais. O resultado será um valor booleano
+             * 
+             * == : igual a
+             * != : diferente de
+             *  < : menor que
+             *  <= : menor ou igual a
+             *   > : maior
+             *   >= : maior ou igual a
+             */
+
+            int quantidadeCarros = 60;
+
+            Console.WriteLine($"Quantidade de carros é igual a 60? {quantidadeCarros == 60}");
+            Console.WriteLine($"Quantidade de carros é diferente que 60? {quantidadeCarros != 60}");
+            Console.WriteLine($"Quantidade de carros é menor que 40? {quantidadeCarros < 40}");
+            Console.WriteLine($"Quantidade de carros é menor ou igual a 70? {quantidadeCarros <= 70}");
+            Console.WriteLine($"Quantidade de carros é maior que 80? {quantidadeCarros > 80}");
+            Console.WriteLine($"Quantidade de carros é maior ou igual a 56? {quantidadeCarros >= 56}");
+                
+            Console.ReadKey();
+        }
+    }
+}
