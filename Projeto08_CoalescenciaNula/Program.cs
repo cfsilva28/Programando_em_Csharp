79a0be0fd4c5861fff89c35dd56227b5ab59721a
cfsilva28 <ceferreira@gmail.com>
2023-01-11 19:33:20 -0300

    Operador de Coalescencia Nula

---
+++ Projeto08_CoalescenciaNula/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto08_CoalescenciaNula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * O operador de coalescencia nula retornará o valor do operando da EDSQUERDA se não for NULL
+             * 
+             * Caso o contrário, ele retornará o valor da DIREITA
+             * 
+             */
+
+            string nomeCurso = null;
+
+            Console.WriteLine($"{nomeCurso ?? "Nome do curso não informado"}");
+
+            Console.ReadKey();
+        }
+    }
+}
