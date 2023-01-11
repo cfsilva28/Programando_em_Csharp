93f8915db3a4f94d0428416747638c000df099de
cfsilva28 <ceferreira@gmail.com>
2023-01-10 22:46:36 -0300

    Operador Ternário

---
+++ Projeto07_OperadorTernario/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto07_OperadorTernario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Operador Ternário
+             * 
+             * teste lógico ? resultado se verdadeiro : resultado se falso
+             * 
+             * teste lógico: qualquer expressão que possa ser avaliada como verdadeira ou falsa
+             * 
+             * resultado se verdadeiro: é o valor atribuído se o teste lógico for avaliado como verdadeiro
+             * 
+             * resultado se falso: é o valor atribuído se o teste lógico for avaliado como falso
+             * 
+             */
+
+            int quantidadeVendas = 510;
+            string avaliacao = quantidadeVendas % 2 == 0 ? "par" : "ímpar";
+            Console.WriteLine($"A quantidade de vendas {quantidadeVendas} é {avaliacao} ");
+
+            Console.ReadKey();
+        }
+    }
+}
