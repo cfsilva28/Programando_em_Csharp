afdab4594b09dfb26e0c56bef68e97d015b60278
cfsilva28 <ceferreira@gmail.com>
2023-01-11 20:55:48 -0300

    Formatação

---
+++ Projeto10_Formatacao/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Projeto10_Formatacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Formatação de Caracteres
+             * 
+             * c: monetário
+             * e: notação científica
+             * f: fixo - ponto flutuante
+             * g: geral
+             * p: percentual
+             * x: hexadecimal
+             * 
+             */
+
+            string produto = "Notebook";
+
+            decimal preco = 9650.90m;
+
+            /*
+             * Formatação Monetária
+             * 
+             */
+
+            Console.WriteLine($"Produto: {produto}\nPreço: {preco:c}");
+
+            Console.WriteLine();
+
+            /*
+             * CultureInfo
+             * 
+             * Altera as configurações regionais da aplicação
+             * 
+             */
+
+            decimal valorMonitor = 1210.50m;
+
+            Console.WriteLine($"Valor do monitor em dólar: {valorMonitor.ToString("c", new CultureInfo("en-US"))}");
+
+
+            /*
+             * Formatação de Data e Hora
+             * 
+             * 
+             */
+
+            DateTime dataAtual = DateTime.Now;
+
+            Console.WriteLine($"Data Atual: {dataAtual:d}");
+            Console.WriteLine($"Data Atual Completa: {dataAtual:D}");
+            Console.WriteLine($"Data e Hora Atuais: {dataAtual:g}");
+            Console.WriteLine($"Ano Atual: {dataAtual:yyyy}");
+            Console.WriteLine($"Mês Atual: {dataAtual:MM}");
+            Console.WriteLine($"Dia Atual: {dataAtual:dd}");
+            Console.WriteLine($"Hora Atual Completa: {dataAtual:T}");
+            Console.WriteLine($"Hora Atual Completa: {dataAtual:HH}");
+            Console.WriteLine($"Minuto Atual: {dataAtual:mm}");
+            Console.WriteLine($"Segundo Atual: {dataAtual:ss}");
+
+
+            /*
+             * 
+             * Formatação Hexidecimal
+             */
+
+            int massaObjeto = 500;
+
+            Console.WriteLine($"Massa do objeto em hexadecimal: {massaObjeto:x}");
+
+            /*
+             * Formatação Científica
+             * 
+             */
+
+            double velocidadeDaLuz = 299792.4; //km/s
+
+            Console.WriteLine($"Velocidade da luz em notação científica: {velocidadeDaLuz:e2}");
+
+            Console.WriteLine();
+
+            /*
+             * Formatação Percentual
+             */
+
+            double descontoCompra = 0.055;
+
+            Console.WriteLine($"Desconto na compra: {descontoCompra:p2}");
+
+            Console.WriteLine();
+
+            float massaParticula = 0.00987654321f;
+
+            Console.WriteLine($"Massa da particula: {massaParticula:c5}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projeto10_Formatacao/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Projeto10_Formatacao")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("UOL")]
+[assembly: AssemblyProduct("Projeto10_Formatacao")]
+[assembly: AssemblyCopyright("Copyright © UOL 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2963a399-c168-43cf-a8b3-3fb0ffe19e2d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
