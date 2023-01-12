b8c6a06ecac28380bdfd85264e98e24dd503303c
cfsilva28 <ceferreira@gmail.com>
2023-01-12 20:24:16 -0300

    While

---
+++ Projeto03_Modulo02_While/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Quando não é sabido quantas vezes um determinado bloco de instruções deverá ser executado, utiliza-se o looop while
+ * 
+ */
+
+
+int num = 1000;
+
+while (num <= 5000)
+{
+
+    if (num == 3000)
+    {
+
+        num += 1000;
+
+        continue; // Caso o valor acima na condição for verdadeiro o "continue" ignora as demais ações abaixo dele 
+                  // Fazendo a operação iniciar novamente da primeira etapa.
+    }
+
+    if (num == 5000)
+    {
+        break;
+    }
+    Console.WriteLine($"Número {num}");
+
+    num += 1000;
+}
+
+Console.ReadKey();
