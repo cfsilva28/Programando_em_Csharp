6770e1c416129f30377283f58a757b371c2b12d2
cfsilva28 <ceferreira@gmail.com>
2023-01-11 22:03:44 -0300

    Concatenação

---
+++ Projeto13_concatenacaoDeStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto13_concatenacaoDeStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome = "Julio ";
+            string sobreNome =  "Cesar";
+
+            string nomeCompleto = String.Concat(nome, sobreNome);
+
+
+
+            Console.WriteLine($"Nome Completo: {nomeCompleto}");
+
+            Console.ReadKey();
+        }
+    }
+}
