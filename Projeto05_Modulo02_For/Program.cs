a5591f0b8f86d9b419a6386e0fa2e52a179843da
cfsilva28 <ceferreira@gmail.com>
2023-01-12 20:57:21 -0300

    For

---
+++ Projeto05_Modulo02_For/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ *  O loop for deve ser utilizado quando é sabido quantas vezes um bloco de instruções deve ser repetido.
+ */
+
+// 5 a 20 em ordem crescente
+
+Console.WriteLine("Números de 5 a 20 em ordem crescente\n");
+
+for (int a = 5; a <= 20; a++)
+{
+    Console.WriteLine($"Valor de 'a': {a}");
+}
+
+Console.WriteLine();
+
+//5 a 20 em ordem decrescente
+
+Console.WriteLine("Números de 5 a 20 em ordem decrescente\n");
+
+for (int a = 20; a > 5; a--)
+{
+    Console.WriteLine($"Valor de 'a': {a}");
+}
+
+Console.ReadKey();
