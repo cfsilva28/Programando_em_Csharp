66d4d6779269e8c2b3510359fe1e901488a0ba60
cfsilva28 <ceferreira@gmail.com>
2023-01-13 20:57:07 -0300

    Propriedades

---
+++ Projeto_01_Modulo03_Classes/Projeto02_Modulo03_Propriedades/Class/Automovel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto02_Modulo03_Propriedades.Class
+{
+
+    /*
+     * Uma propriedade é um valor que a classe disponibiliza para leitura e gravação
+     * 
+     * get: leitura da propriedade
+     * set: escrita da prpopriedade
+     * 
+     */
+
+    internal class Automovel
+    {
+
+        //Campos
+        private string? _corCarro;
+
+        private decimal _valorCarro;
+
+        private string? _placaCarro;
+
+        //Propriedade
+        public string? Modelo { get; set; }
+
+        public string? Fabricante { get; set; }
+
+        public short Ano { get; }
+
+        public string Placa
+        {
+
+            get
+            {
+
+                return _placaCarro;
+
+            }
+            set
+            {
+
+                _placaCarro = value;
+
+            }
+
+        }
+
+        public decimal Valor
+        {
+
+            get
+            {
+
+                return _valorCarro;
+
+            }
+
+        }
+
+        public string Cor
+        {
+
+            set
+            {
+
+                _corCarro = value;
+
+            }
+        }
+
+    }
+}
---
+++ Projeto_01_Modulo03_Classes/Projeto02_Modulo03_Propriedades/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Projeto02_Modulo03_Propriedades.Class;
+
+Automovel automovel = new();
+
+automovel.Fabricante = "Jeep"; //set
+automovel.Modelo = "Renegade"; //set
+automovel.Cor = "Preto";       //set
+
+Console.WriteLine(automovel.Fabricante); //get
