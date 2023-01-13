a63f1a4373aa47d672bde18161fff16b355733b8
cfsilva28 <ceferreira@gmail.com>
2023-01-13 20:17:26 -0300

    Modulo 03 Classes

---
+++ Projeto_01_Modulo03_Classes/Projeto_01_Modulo03_Classes/Class/Automovel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto01_Modulo03_Classes.Class
+{
+
+    /*
+     * Clasee é um modelo que representa um conjunto de elementos (pessoas, objetos, etc) com características comuns
+     * 
+     * A classe é um abstração de um possível elemento
+     */
+
+    /*
+     * Tipos de modificadores
+     * 
+     * public: o acesso é livre quando esse modificador é utilizado
+     * 
+     * private: é a visibilidade definida para métodos e campos da própria classe
+     * 
+     * protected: apenas a classe que contém modificadore e os tipos derivados dessa classe possuem acesso
+     * 
+     * internal: limita o acesso apenas ao assembly atual
+     * 
+     * protected internal: o acesso é limitado ao assembly atual e aos tipos derivados da classe que contém o modificador
+     * 
+     * 
+     */
+
+    /*
+     * 
+     * Valores padrão dos atributos (membros, campos)de uma classe
+     * 
+     * int: 0
+     * boolean: false
+     * string: null 
+     *     
+     */
+
+    /*
+     * Atríbutos
+     * 
+     * Características específicas de um objeto
+     * 
+     * Para poder definir e controlar todas características que os objetos podem ter, a classe as armazena em propriedades
+     */
+
+
+    internal class Automovel
+    {
+        public string Fabricante;
+        public string Modelo;
+        public string Placa;
+        public string Cor;
+        public short Ano;
+    }
+}
---
+++ Projeto_01_Modulo03_Classes/Projeto_01_Modulo03_Classes/Program.cs
@@ -0,0 +1,39 @@
+﻿using Projeto01_Modulo03_Classes.Class;
+
+Automovel carro = new Automovel();
+
+Console.WriteLine("Primeiro Carro");
+
+carro.Ano = 2021;
+carro.Cor = "Branco";
+carro.Fabricante = "Honda";
+carro.Modelo = "Civic";
+carro.Placa = "ABC1D23";
+
+Console.WriteLine($"Fabricante: {carro.Fabricante}");
+Console.WriteLine($"Modelo: {carro.Modelo}");
+Console.WriteLine($"Placa: {carro.Placa}");
+Console.WriteLine($"Cor: {carro.Cor}");
+Console.WriteLine($"Cor: {carro.Ano}");
+
+Console.WriteLine(new String('-', 60));
+
+Console.WriteLine();
+
+Console.WriteLine("Segundo Carro\n");
+
+Automovel carro2 = new Automovel
+{
+    Ano = 2022,
+    Cor = "Prata",
+    Modelo = "Corolla",
+    Placa = "FGH5X65"
+};
+
+Console.WriteLine($"Fabricante: {carro2.Fabricante}");
+Console.WriteLine($"Modelo: {carro2.Modelo}");
+Console.WriteLine($"Placa: {carro2.Placa}");
+Console.WriteLine($"Cor: {carro2.Cor}");
+Console.WriteLine($"Cor: {carro2.Ano}");
+
+Console.ReadKey();
