932a34b8f998b07a8d3de207ff55253f1cffcb18
cfsilva28 <ceferreira@gmail.com>
2023-01-12 23:00:18 -0300

    Exercício 2 Gerador de Senha

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Gerar Senha
+ * 
+ * A senha deve ter entre 4 e 10 dígitos
+ * 
+ * A quantidade de dígitos deve ser par
+ * 
+ */
+
+Console.Write("Informe a quantidade de dígitos da senha: ");
+
+int digitos;
+
+try
+{
+
+    digitos = Convert.ToInt32(Console.ReadLine());
+
+    if (digitos < 4 || digitos > 10 || digitos % 2 != 0)
+    {
+
+        Console.WriteLine("A quantidade de dígitos é inválida");
+
+        Console.ReadKey();
+
+        return;
+
+    }
+
+    string senha = "";
+
+    var randomico = new Random();
+
+    for (int i = 1; i <= digitos; i++)
+    {
+
+        int numero = randomico.Next(0, 9);
+
+        senha += numero; //senha = senha + numero
+
+    }
+
+    Console.WriteLine($"Senha gerada: {senha}");
+
+}
+catch (Exception e)
+{
+
+    Console.WriteLine(e.Message);
+
+}
+
+Console.ReadKey();
