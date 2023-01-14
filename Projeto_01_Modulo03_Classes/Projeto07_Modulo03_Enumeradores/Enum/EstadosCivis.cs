4e4333f772ca02f01a93bcedf5fb82f179da62fc
cfsilva28 <ceferreira@gmail.com>
2023-01-13 22:57:04 -0300

    Enumeradores

---
+++ Projeto_01_Modulo03_Classes/Projeto07_Modulo03_Enumeradores/Enum/EstadosCivis.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto07_Modulo03_Enumeradores.Enum
+{
+    /*
+         * Enum
+         * 
+         * É utilizado para declarar um conjunto de constantes. Um enumerador não pode herdadar ou ser herdado
+         * 
+         * O tipo padrão de enumerador é um int
+         */
+    internal enum EstadosCivis
+    {
+        Casado, Solteiro, Viúvo, Divorciado, Outro
+    }
+}
---
+++ Projeto_01_Modulo03_Classes/Projeto07_Modulo03_Enumeradores/Program.cs
@@ -0,0 +1,23 @@
+﻿using Projeto07_Modulo03_Enumeradores.Enum;
+
+Console.WriteLine("Todos os estados civis");
+
+//Loop para ler todos os estados civis
+
+foreach(EstadosCivis i in Enum.GetValues(typeof(EstadosCivis)))
+{
+    Console.WriteLine($"Estado Civil: {i}");
+}
+Console.WriteLine();
+
+Console.WriteLine(new string('-', 50));
+
+Console.WriteLine();
+
+
+//Enum para string;
+
+string casado = $"Estado civil: {EstadosCivis.Casado}";
+
+Console.WriteLine(casado);
+Console.ReadKey();
