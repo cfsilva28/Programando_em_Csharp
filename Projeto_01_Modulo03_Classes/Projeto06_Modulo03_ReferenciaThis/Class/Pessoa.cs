92748731c1e53aadf9da05a3c33b997232b594e9
cfsilva28 <ceferreira@gmail.com>
2023-01-13 22:32:17 -0300

    Referência This

---
+++ Projeto_01_Modulo03_Classes/Projeto06_Modulo03_ReferenciaThis/Class/Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto06_Modulo03_ReferenciaThis.Class
+{
+    /*
+     * Referência This
+     * 
+     * Quando os membros são acessados dentro da PRÓPRIA CLASSE, é através de uma referência, chamada THIS
+     * que estes membros são referenciados
+     * 
+     */
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public byte Idade { get; set; }
+        public char Genero { get; set; }
+
+
+        public void ObterDados(string Nome, byte Idade, char Genero)
+        {
+            this.Nome = Nome;
+            this.Idade = Idade;
+            this.Genero = Genero;
+        }
+
+    }
+}
---
+++ Projeto_01_Modulo03_Classes/Projeto06_Modulo03_ReferenciaThis/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Projeto06_Modulo03_ReferenciaThis.Class;
+
+Pessoa pessoa = new Pessoa();
+
+pessoa.ObterDados("Lorran", 18, 'M');
+
+Console.WriteLine($"Nome: {pessoa.Nome}");
+Console.WriteLine($"Idade: {pessoa.Idade}");
+Console.WriteLine($"Gênero: {pessoa.Genero}");
+
+Console.ReadKey();
