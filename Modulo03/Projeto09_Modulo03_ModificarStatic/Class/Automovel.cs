f6382176f1973067606a30a6e86c4b78069a5390
cfsilva28 <ceferreira@gmail.com>
2023-01-16 20:26:57 -0300

    Modificar Static

---
+++ Modulo03/Projeto09_Modulo03_ModificarStatic/Class/Automovel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto09_Modulo03_ModificarStatic.Class
+{
+    internal class Automovel
+    {
+
+        public static string Fabricante { get; set; }
+
+        public static string Modelo { get; set; }
+
+        public string ExibirDados()
+        {
+
+            return $"Fabricante: {Fabricante}\nModelo: {Modelo}";
+        }
+    }
+
+}
---
+++ Modulo03/Projeto09_Modulo03_ModificarStatic/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Projeto09_Modulo03_ModificarStatic.Class;
+
+Automovel automovel = new Automovel();
+
+Automovel.Fabricante = "VolksWagen";
+Automovel.Modelo = "T-Cross";
+
+Console.WriteLine(automovel.ExibirDados());
+
+Console.ReadKey();
