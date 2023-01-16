423cb1ab0fc128dcac29d02131ba0a122190e9c3
cfsilva28 <ceferreira@gmail.com>
2023-01-16 19:33:09 -0300

    Estruturas

---
+++ Modulo03/Projeto08_Modulo03_Estruturas/Projeto08_Modulo03_Estruturas/Program.cs
@@ -0,0 +1,15 @@
+﻿using Projeto08_Modulo03_Estruturas.Struct;
+
+Endereco endereco = new Endereco()
+{
+    Rua = "Avenida Paulista, 1009",
+    Bairro = "Cerqueira César",
+    Cidade = "São Paulo",
+    UF = "SP",
+    CEP = "01311-919"
+};
+
+Console.WriteLine(endereco.ExibirEndereco());
+
+Console.ReadKey();
+
---
+++ Modulo03/Projeto08_Modulo03_Estruturas/Projeto08_Modulo03_Estruturas/Struct/Endereco.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto08_Modulo03_Estruturas.Struct
+{
+    /*
+     * Uma estrutura é um tipo valor
+     * 
+     * A instância de uma estrutura é chamada value e fica armazenada na memória stack
+     * 
+     * Comparando classe e estrutura:
+     * 
+     * Uma classe é um tipo referência. A instância de uma classe é chamada object e fica armazenada na memória heap
+     * 
+     * A classe permite herança e polimorfismo
+     * Link:
+     */
+    internal struct Endereco
+    {
+        public string Rua { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public string CEP { get; set; }
+        public string ExibirEndereco()
+        {
+            return $"Rua: {Rua}\nBairro: {Bairro}\nCidade: {Cidade}\nUF: {UF}\nCEP: {CEP}";
+        }
+    }
+}
