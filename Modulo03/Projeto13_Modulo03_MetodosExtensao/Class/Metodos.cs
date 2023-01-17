b1cc0e3df251d10428fb81c3fead36281e748d44
cfsilva28 <ceferreira@gmail.com>
2023-01-16 22:46:22 -0300

    Metodos de Extensão

---
+++ Modulo03/Projeto13_Modulo03_MetodosExtensao/Class/Metodos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto13_Modulo03_MetodosExtensao.Class
+{
+    internal static class Metodos
+    {
+        /*
+         * Ao se utilizar um método de extensão é possível ampliar uma classe ou uma estrutura já existente com métodos
+         * estáticos adicionais
+         */
+        public static string RetirarAcentos(this string texto)
+        {
+            string comAcentos = "ÄÅÁÂÀÃäáâàãÉÊËÈéêëèÍÎÏÌíîïìÖÓÔÒÕöóôòõÜÚÛüúûùÇç";
+            string semAcentos = "AAAAAAaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUuuuuCc";
+
+            for (int i = 0; i < comAcentos.Length; i++)
+            {
+                texto = texto.Replace(comAcentos[i], semAcentos[i]);
+            }
+            return texto;
+        }
+    }
+}
---
+++ Modulo03/Projeto13_Modulo03_MetodosExtensao/Program.cs
@@ -0,0 +1,8 @@
+﻿using Projeto13_Modulo03_MetodosExtensao.Class;
+
+string texto = "São cabeçalhos, rodapés, construções de gráficos";
+
+string textoTratado = texto.RetirarAcentos();
+
+Console.WriteLine(textoTratado);
+Console.ReadKey();
