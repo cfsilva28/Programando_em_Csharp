351749f99a27cd5c440b7160fd9345631f0ac92c
cfsilva28 <ceferreira@gmail.com>
2023-01-16 22:30:34 -0300

    Métodos Estáticos

---
+++ Modulo03/Projeto12_modulo03_MetodosEstaticos/Class/Metodos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto12_modulo03_MetodosEstaticos.Class
+{
+    /*
+     * Métodos Estáticos
+     * 
+     * Não faz uso da instância à qual pertence para ser utilizado
+     */
+    internal class Metodos
+    {
+        public static string RetirarAcentos(string texto)
+        {
+            string comAcentos = "ÄÅÁÂÀÃäáâàãÉÊËÈéêëèÍÎÏÌíîïìÖÓÔÒÕöóôòõÜÚÛüúûùÇç";
+            string semAcentos = "AAAAAAaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUuuuuCc";
+
+            for (int i = 0; i < comAcentos.Length; i++)
+            {
+                texto = texto.Replace(comAcentos[i], semAcentos[i]);
+            }
+            return texto;
+        }
+    }
+}
---
+++ Modulo03/Projeto12_modulo03_MetodosEstaticos/Program.cs
@@ -0,0 +1,10 @@
+﻿using Projeto12_modulo03_MetodosEstaticos.Class;
+
+
+string texto = "São cabeçalhos, rodapés, construções de gráficos";
+
+string textoSemAcentos = Metodos.RetirarAcentos(texto);
+
+Console.WriteLine(textoSemAcentos);
+
+Console.ReadKey();
