1d8d01d4e1f77b4bb386d88bb69b61b9c0f7af5e
cfsilva28 <ceferreira@gmail.com>
2023-01-16 21:25:23 -0300

    Sobrecarga de Método

---
+++ Modulo03/Projeto11_Modulo03_SobrecargaMetodos/Class/Pessoa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto11_Modulo03_SobrecargaMetodos.Class
+{
+
+    /*
+     * Sobrecarga de Métodos
+     * 
+     * É a possibilidade de fazer com que uma mesma operação seja executada de formas diferentes
+     * 
+     * Quando tem-se implementações de MESMO NOME, mas com NÚMERO DIFERENTE DE PARÂMETROS ou
+     * PARÂMETROS DE TIPOS DIFERENTES, pode-se SOBRECARREGAR um método
+     */
+    internal class Pessoa
+    {
+        public string? Nome { get; set; }
+
+        public byte Idade { get; set; }
+
+        public string ExibirDados()
+        {
+            return $"Nome: {Nome}\nIdade: {Idade} anos";
+        }
+
+        public string ExibirDados(string observacao)
+        {
+            return $"Nome: {Nome}\nIdade: {Idade} anos\nObservacao: {observacao}";
+        }
+    }
+}
---
+++ Modulo03/Projeto11_Modulo03_SobrecargaMetodos/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Projeto11_Modulo03_SobrecargaMetodos.Class;
+
+Pessoa pessoa = new Pessoa();
+
+pessoa.Nome = "Andrea";
+pessoa.Idade = 25;
+
+Console.WriteLine(pessoa.ExibirDados());
+
+Console.WriteLine();
+
+Console.WriteLine(new string ('-', 50));
+
+Console.WriteLine(pessoa.ExibirDados("Aluna do Curso de C#"));
+Console.ReadKey();
