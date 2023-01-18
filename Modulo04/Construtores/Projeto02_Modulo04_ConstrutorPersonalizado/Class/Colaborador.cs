6d6f46885f6d994f2b28a7dc45b9714aa0628f99
cfsilva28 <ceferreira@gmail.com>
2023-01-18 19:57:31 -0300

    Construtor Personalizado

---
+++ Modulo04/Construtores/Projeto02_Modulo04_ConstrutorPersonalizado/Class/Colaborador.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto02_Modulo04_ConstrutorPersonalizado.Class
+{
+    internal class Colaborador
+    {
+
+        public string Nome { get; set; } = "Não informado";
+
+        public string Cargo { get; set; } = "Não informado";
+
+        public decimal Salario { get; set; } = 0;
+
+        //Construtor Default
+        public Colaborador() { }
+
+        //Construtor Personalizado
+        public Colaborador(string nome, string cargo, decimal salario)
+        {
+
+            Nome = nome;
+            Cargo = cargo;
+            Salario = salario;
+
+        }
+
+        public string DadosColaborador()
+        {
+
+            return $"Nome: {Nome}\nCargo: {Cargo}\nSalário: {Salario:c}";
+
+        }
+
+    }
+}
---
+++ Modulo04/Construtores/Projeto02_Modulo04_ConstrutorPersonalizado/Program.cs
@@ -0,0 +1,9 @@
+﻿//Colaborador colaborador = new Colaborador("Sofia","Médica",25000);
+
+using Projeto02_Modulo04_ConstrutorPersonalizado.Class;
+
+Colaborador colaborador = new Colaborador();
+
+Console.WriteLine(colaborador.DadosColaborador());
+
+Console.ReadKey();
