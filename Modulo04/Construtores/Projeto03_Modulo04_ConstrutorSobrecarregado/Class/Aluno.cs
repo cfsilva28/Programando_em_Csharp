6dc7412eeedd24b4b9d888ad5ba56d1d15be0bcc
cfsilva28 <ceferreira@gmail.com>
2023-01-18 20:45:52 -0300

    Construtores Sobrecarregado

---
+++ Modulo04/Construtores/Projeto03_Modulo04_ConstrutorSobrecarregado/Class/Aluno.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto03_Modulo04_ConstrutorSobrecarregado.Class
+{
+    /*
+     * Sobrecarga de construtores
+     * 
+     * Utilizado quando nem todas as propriedades de uma classe são requeridas
+     * 
+     */
+
+    internal class Aluno
+    {
+
+        public string? Nome { get; set; }
+
+        public int Idade { get; set; }
+
+        public double Peso { get; set; }
+
+        public double Altura { get; set; }
+
+        //Construtor
+        public Aluno(double peso, double altura)
+        {
+
+            Peso = peso;
+            Altura = altura;
+
+        }
+
+        //Construtor Sobrecarregado
+        public Aluno(string nome, double peso, double altura) : this(peso, altura)
+        {
+
+            Nome = nome;
+
+        }
+
+        //Construtor Sobrecarregado
+        public Aluno(int idade, double peso, double altura) : this(peso, altura)
+        {
+
+            Idade = idade;
+
+        }
+
+        //Construtor Sobrecarregado
+        public Aluno(string nome, int idade, double peso, double altura) : this(idade, peso, altura)
+        {
+
+            Nome = nome;
+
+        }
+
+        public string DadosAluno()
+        {
+
+            return $"Nome: {Nome}\nIdade: {Idade}\nPeso: {Peso}\nAltura: {Altura}";
+
+        }
+
+    }
+}
---
+++ Modulo04/Construtores/Projeto03_Modulo04_ConstrutorSobrecarregado/Program.cs
@@ -0,0 +1,13 @@
+﻿using Projeto03_Modulo04_ConstrutorSobrecarregado.Class;
+
+Aluno aluno1 = new Aluno("Gabriela", 25, 65, 1.7);
+
+Aluno aluno2 = new Aluno("Maria", 58, 1.65);
+
+Console.WriteLine(aluno1.DadosAluno());
+
+Console.WriteLine();
+
+Console.WriteLine(aluno2.DadosAluno());
+
+Console.ReadKey();
