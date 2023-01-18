3e538aa15ed16aa35b348362843fc20a3cf27271
cfsilva28 <ceferreira@gmail.com>
2023-01-17 22:16:06 -0300

    Constantes

---
+++ Modulo03/Projeto18_Modulo03_Const/Class/Constantes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto18_Modulo03_Const.Class
+{
+    /*
+     * Constantes
+     * 
+     * Prefixando o campo com a palavra CONST, indica que é um campo estático (STATIC)
+     * 
+     * O valor da constante nunca poderá ser alterado
+     */
+    internal class Constantes
+    {
+        public const string SERVIDOR = "192.168.0.201";
+
+        public const int VELOCIDADE_LUZ = 30000;
+    }
+}
---
+++ Modulo03/Projeto18_Modulo03_Const/Program.cs
@@ -0,0 +1,10 @@
+﻿using Projeto18_Modulo03_Const.Class;
+
+string servidor = Constantes.SERVIDOR;
+
+int velocidadeLuz = Constantes.VELOCIDADE_LUZ;
+
+servidor = "192.168.1.105";
+
+Console.WriteLine($"Endereço do servidor: {servidor}");
+Console.WriteLine($"Velocidade da Luz: {velocidadeLuz}");
