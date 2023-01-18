3ea63a3e5b3a531f42aa7c77809f384855a1704f
cfsilva28 <ceferreira@gmail.com>
2023-01-17 21:36:39 -0300

    Parâmetros Opcionais Default

---
+++ Modulo03/Projeto16_Modulo03_ParametrosOpcionaisDefault/Enum/Estados.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto16_Modulo03_ParametrosOpcionaisDefault.Class
+{
+    internal enum Estados
+    {
+
+        SP, MG, RJ, RS, SC, PR, PE, BA, MA
+
+    }
+}
---
+++ Modulo03/Projeto16_Modulo03_ParametrosOpcionaisDefault/Program.cs
@@ -0,0 +1,40 @@
+﻿//Método com parâmetro opcional
+
+using Projeto16_Modulo03_ParametrosOpcionaisDefault.Class;
+//Método com parâmetro opcional
+static string MostrarEstado(Estados estado = Estados.PR)
+{
+
+    return $"O estado informado foi {estado}";
+
+}
+
+//Método com parâmetros default
+static double CalcularValor(double valor, double taxa = 0, double imposto = 0)
+{
+
+    double desc1 = valor * taxa / 100;
+    double desc2 = valor * imposto / 100;
+    double valorLiquido = valor - desc1 - desc2;
+
+    return valorLiquido;
+
+}
+
+//Exibe resultados de métodos com parâmetro opcional
+Console.WriteLine(MostrarEstado(Estados.MG));
+
+Console.WriteLine();
+
+Console.WriteLine(new string('-', 60));
+
+Console.WriteLine();
+
+double valor = 1000;
+
+Console.WriteLine($"Valor sem desconto: {CalcularValor(valor)}");
+Console.WriteLine($"Valor com desconto de taxa: {CalcularValor(taxa: 10, valor: valor)}");
+Console.WriteLine($"Valor com desconto de imposto: {CalcularValor(imposto: 18, valor: valor)}");
+Console.WriteLine($"Valor com desconto de taxa e impostos: {CalcularValor(imposto: 18, taxa: 10, valor: valor)}");
+
+Console.ReadKey();
