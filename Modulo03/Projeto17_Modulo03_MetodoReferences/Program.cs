b16ce342c7e7f70f12974a2a9d3a764bc0836fcb
cfsilva28 <ceferreira@gmail.com>
2023-01-17 22:02:12 -0300

    Méthod References

---
+++ Modulo03/Projeto17_Modulo03_MetodoReferences/Program.cs
@@ -0,0 +1,14 @@
+﻿
+static double Calcular(double valor) => valor * 0.10;
+
+static void Mensagem() => Console.WriteLine("Method References.");
+
+Mensagem();
+
+Console.WriteLine();
+
+double valor = 1000;
+
+Console.WriteLine($"10% de {valor}: {Calcular(valor)}");
+
+Console.ReadKey();
