a5a667ee058b16ce83fa151a1ab09f9b9eec85f4
cfsilva28 <ceferreira@gmail.com>
2023-01-20 20:08:13 -0300

    Array Bidimensional

--- Modulo08/Coleções/Projeto01_Array/Program.cs
+++ Modulo08/Coleções/Projeto01_Array/Program.cs
@@ -27,4 +27,27 @@ for (int i = 0; i < numeros1.Length; i++)
 {
     Console.WriteLine($"{numeros1[i]}");
 }
+
+Console.WriteLine();
+//Declaração de um array bidimensional
+
+int[,] notas = { { 17, 22, 12, 44, 34 }, { 23, 33, 12, 31, 45 }, { 2, 45, 35, 1, 22 } };
+
+int linhas = notas.GetLength(0);
+
+int colunas = notas.GetLength(1);
+
+Console.WriteLine();
+
+Console.WriteLine("Valores do array bidimensional");
+
+for (int i = 0; i < linhas; i++)
+{
+    for (int j = 0; j < colunas; j++)
+    {
+        Console.Write($" {notas[i, j]}");
+    }
+    Console.WriteLine("\n");
+}
+
 Console.ReadKey();
