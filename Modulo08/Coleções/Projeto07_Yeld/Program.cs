291c1a7416823dbeca09c1ccf13bc4ccce5e9df5
cfsilva28 <ceferreira@gmail.com>
2023-01-20 22:45:26 -0300

    IEnumerable

---
+++ Modulo08/Coleções/Projeto07_Yeld/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * IEnumerable
+ * 
+ * Não permite alterações na coleção, ou seja, funciona apenas como leitura
+ * 
+ * Não fornece nenhuma informação sobre a coleção
+ * 
+ * Não permite acesso aleatório às posições, ou seja, somente é possível percorrer a coleção sequencialmente
+ * 
+ * Indicado para consultar dados a partir de coleções como List<T> e Array
+ * 
+ */
+
+List<string> nomes = new List<string>()
+{ "Andrea", "Cesar", "Diogo", "Jonathan", "Lincoln", "Lorran", "Paulo", "Pedro", "Rafael", "Obelix"};
+
+IEnumerable<string> nomesIEnumerable = nomes;
+
+foreach (var nome in nomesIEnumerable)
+{
+
+    Console.WriteLine(nome);
+
+}
+
+Console.ReadKey();
