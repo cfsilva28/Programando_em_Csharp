3e0e65b8632fcadd820a07bb74a98261e929ed1f
cfsilva28 <ceferreira@gmail.com>
2023-01-23 20:36:47 -0300

    Subclasse

---
+++ Modulo07/Genericos/Projeto04_Subclasse/Class/Subclasse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto04_Subclasse.Class
+{
+
+    /*
+     * Se, na subclasse, o tipo não for especificado na superclasse, durante o processo de herança, isso 
+     * significa que a subclasse deve ser genérica
+     *  
+     * A sublcasse está repassando o parâmetro de tipo para a superclasse. Quando instanciada, a Sublcasse deverá
+     * receber seu tipo adequadamente 
+     */
+    internal class Subclasse<T> : ClasseBase<decima>
+    {
+    }
+}
---
+++ Modulo07/Genericos/Projeto04_Subclasse/Program.cs
@@ -0,0 +1,5 @@
+﻿using Projeto04_Subclasse.Class;
+
+Console.WriteLine("Subclasse Genérica");
+
+Subclasse<string> sub = new Subclasse<string>();
