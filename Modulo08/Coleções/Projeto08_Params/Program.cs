0d2d8e2fff203b2fa31acac63bf7784097353a98
cfsilva28 <ceferreira@gmail.com>
2023-01-23 19:24:41 -0300

    Params

---
+++ Modulo08/Coleções/Projeto08_Params/Program.cs
@@ -0,0 +1,15 @@
+﻿static decimal CalcularMedia(params decimal[] numeros)
+{
+    decimal soma = 0;
+
+    for (int i = 0; i < numeros.Length; i++)
+    {
+        soma += numeros[i]; // soma = soma + numeros[i]
+    }
+
+    return soma / numeros.Length;
+}
+
+Console.WriteLine($"Média: {CalcularMedia(1,2,3,4,5,6)}");
+
+Console.ReadKey();
