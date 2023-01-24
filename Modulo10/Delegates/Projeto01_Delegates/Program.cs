fb5930f481c0143e59c8102f4bcfffdcff865573
cfsilva28 <ceferreira@gmail.com>
2023-01-24 19:55:16 -0300

    Delegates + Expressões Lambda

---
+++ Modulo10/Delegates/Projeto01_Delegates/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Projeto01_Delegates
+{
+    /*
+    * Delegates
+    * 
+    * Fazem referências a métodos ou operações
+    *   
+    */
+
+    /*
+    * Expressão Lambda
+    *
+    *Sintaxe
+    * (parâmetros) => resultado. Leia-se: uma lista de parâmetros produz o resultado esperado
+    * 
+    */
+    internal class Program
+    {
+
+
+
+        public delegate double Calcular(double x, double y);
+
+        public delegate void Mensagem(string texto);
+
+
+        static void Main(string[] args)
+        {
+            //Declaração de um delegate
+            Calcular multiplicacao = new Calcular((x, y) => x * y);
+
+            double resultado = multiplicacao(10, 5);
+
+            Console.WriteLine($"Resultado da multiplicação: { resultado}");
+
+            Console.ReadKey();
+        }
+    }
+}
