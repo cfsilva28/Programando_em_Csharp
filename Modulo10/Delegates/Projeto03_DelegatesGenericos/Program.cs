5a6f7aedf26fc78f4faa80c014eaf87b40cd3b57
cfsilva28 <ceferreira@gmail.com>
2023-01-24 20:35:48 -0300

    Delegates Genéricos

---
+++ Modulo10/Delegates/Projeto03_DelegatesGenericos/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    public delegate bool Buscar<T>(T item);
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Delegates Genéricos");
+
+
+        string[] paises = { "Brasil", "Portugal", "França", "Itália", "Estados Unidos" };
+
+        Buscar<string> resultado = p => p.Contains("i");
+
+        foreach (string pais in paises)
+        {
+            if (resultado(pais))
+            {
+                Console.WriteLine(pais);
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
