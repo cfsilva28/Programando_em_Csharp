1659f10389f587e3886f1f3205b35bd3ebb6f83a
cfsilva28 <ceferreira@gmail.com>
2023-01-23 22:23:02 -0300

    Try Catch

---
+++ Modulo09/TratamentoExecoes/Projeto01_TryCatch/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Try: executa se não houver exeção
+ * 
+ * Catch: captura qualquer exeção encontrada no bloco try
+ * 
+ * Finally: sempre executado ao final do bloco
+ * 
+ */
+
+try
+{
+    Console.WriteLine("informe o numerador: ");
+
+    double numerador = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Informe do denominador: ");
+
+    double denominador = Convert.ToDouble(Console.ReadLine());
+
+    double resultado = numerador / denominador;
+
+    Console.WriteLine($"{numerador} / {denominador}: {resultado}\n\n");
+}
+catch (Exception e)
+{
+    //mensagem personalizada
+    Console.WriteLine("Erro personalizado: Erro ao realizar a operação\n\n");
+
+    //mensagem com os detalhes do erro
+    Console.WriteLine($"Detalhes ténicos: {e.Message}");
+}
+
+finally
+{
+    //é executado independentemente de ter ocorrido uma exeção ou não
+
+    Console.WriteLine("Operação finalizada!");
+}
+
+Console.WriteLine("Fim do programa!");
+Console.ReadKey();
