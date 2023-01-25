4186dbda23a5d57a54c44c04a4ceca7697e8eea3
cfsilva28 <ceferreira@gmail.com>
2023-01-24 21:47:16 -0300

    Delegates

---
+++ Modulo11/Programacao_Assincrona/Projeto01_ProgramacaoAssincronaTask/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Programação Assíncrona
+ * 
+ * Usa-se métodos assíncronos em operações que podem demorar para produzir um resultado (Consulta Bancos de Dados, Webservices etc)
+ * 
+ * Permite a execução do programa na thread principal enquanto um tarefa de longa duração é executada em uma thread independente
+ * 
+ * Thread: uma sequência de instruções que podem ser executadas simultaneamente com outras sequências de instruções
+ * 
+ * Retorno de métodos assíncronos: Task e também Task<T>
+ * 
+ * Task: usado em métodos assíncronos que não retorna um valor. É a promessa de execução de uma tarefa no futuro
+ * 
+ * Task<T>: usado em métodos assíncronos que retorna um valor
+ * 
+ * await: pausa a execução da thread atual até que o método assíncrono seja concluído
+ * 
+ */
+
+class Program
+{
+    private static async Task Aguardar(int tempo)
+    {
+        Console.WriteLine("iniciando tempo de espera...");
+
+        await Task.Delay(tempo);
+
+        Console.WriteLine("Fim do tempo de espera.");
+
+    }
+
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("iniciando o programa, tecle algo para continuar...");
+
+        Console.ReadLine();
+
+        int tempo = 6000;
+
+        await Aguardar(tempo);//milisegundos
+
+        Console.WriteLine($"Passaram-se {tempo} milisegundos");
+
+        Console.WriteLine("Fim do programa. ");
+
+        Console.ReadKey();
+
+
+    }
+
+
+}
+
---
+++ Modulo11/Projeto02ProgramacaoAssincronaTask_Generico/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Programação Assíncrona
+ * 
+ * Task<T>: retorna dados de um método assíncrono (int, double, bool, coleção, classe, etc)
+ * 
+ */
+
+class Program
+{
+
+    static async Task<int> ListarNomes()
+    {
+        var nomes = new List<string> { "Sofia", "Maria", "Pedro", "José" };
+        int total = 0;
+
+        foreach (var nome in nomes)
+        {
+            await Task.Delay(1000);
+
+            if (nome.Contains("a"))
+            {
+                total++;
+            }
+        }
+        return total;
+    }
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Método assíncrono que retorna um valor inteiro");
+
+        Console.WriteLine();
+
+        int total = await ListarNomes();
+
+        Console.WriteLine($"Total de nomes que contém a letra 'a' {total}");
+
+        Console.ReadKey();
+    }
+}
